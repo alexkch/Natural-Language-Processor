2a0a06be4eb2a615971714764708e00475f3e11b
Chang Kao Tsun <c2changk@cdf.toronto.edu>
2016-02-16 07:07:05 -0500

    add error check

--- ibmTest.py
+++ ibmTest.py
@@ -12,6 +12,32 @@
 #		You may also find it helpful to reuse some of your functions from ibmTrain.py.
 #
 
+###IMPORTS##############################################################
+
+import csv, requests, re, json, urllib
+
+# Disable warnings
+requests.packages.urllib3.disable_warnings()
+
+###CONSTANTS############################################################
+
+url = "https://gateway.watsonplatform.net/natural-language-classifier/api/v1/classifiers/"
+test_data = '/u/cs401/A1/tweets/testdata.manualSUBSET.2009.06.14.csv'
+
+
+######### CREDENTIALS ##################################################
+#username = "03f335cf-dbac-4f6f-8a82-211813430928"
+#password = "h6UjcleAHAKs"
+
+
+######### NEW ##########################################################
+username = "88992124-0531-4619-b710-ac512645af6a"
+password = "7HOiCMuUwmKj"
+
+file_size = [500, 2500, 5000]
+
+##############################################
+
 def get_classifier_ids(username,password):
 	# Retrieves a list of classifier ids from a NLClassifier service 
 	# an outputfile named ibmTrain#.csv (where # is n_lines_to_extract).
@@ -28,10 +54,16 @@ def get_classifier_ids(username,password):
 	# Error Handling:
 	#	This function should throw an exception if the classifiers call fails for any reason
 	#
+    
+	response = requests.get(url, data={}, auth=(username, password))	
+	ids = []
+	for classifier in response.json()['classifiers']:
+		ids.append(classifier['classifier_id'])
 	
-	#TODO: Fill in this function
-	
-	return
+	if not response.ok:
+		raise Exception("get Classifier call failed.")
+        
+	return ids
 	
 
 def assert_all_classifiers_are_available(username, password, classifier_id_list):
@@ -52,7 +84,23 @@ def assert_all_classifiers_are_available(username, password, classifier_id_list)
 	#	It should throw an error if any classifier is NOT 'Available'
 	#
 	
-	#TODO: Fill in this function
+	unavaliable = []
+	not_ready = ''
+	
+	for classifier in classifier_id_list:
+		response = requests.get(url + classifier, data={}, auth=(username, password))
+		
+		# if the status is not avaliable, add it to unavaliable list
+		if str(response.json()['status']) != 'Available':
+			unavaliable.append(classifier)
+	
+	# if there are classifiers unavaliable, raise exception
+	if len(unavaliable) > 0:
+	
+		for status in unavaliable:
+			not_ready = not_ready + ' ' + status 
+	
+		raise Exception ("classifier(s):" + not_ready + " is/are not ready")
 	
 	return
 
@@ -83,52 +131,104 @@ def classify_single_text(username,password,classifier_id,text):
 	#
 	# Error Handling:
 	#	This function should throw an exception if the classify call fails for any reason 
-	#
+
+	classification = {}		
 	
-	#TODO: Fill in this function
+	# encode the text into utf-8 for query
+	query = urllib.quote(text.encode("utf-8"))
 	
-	return
+	# throws exception if call fails
+	response = requests.get(url + classifier_id + "/classify?text=" + query, auth=(username, password))
+    
+	if not response.ok:
+		raise Exception("Classifier call failed.")
+
+	classification['top_class'] =  response.json()['top_class']
+	classification['classes'] = response.json()['classes']
+
+	return classification
+	
+
 
 def classify_all_texts(username,password,input_csv_name):
-	# Classifies all texts in an input csv file using all classifiers for a given NLClassifier 
-	# service. 
-	#
-	# Inputs: 
-	# 	username - username for the NLClassifier to be used, as a string
-	#
-	# 	password - password for the NLClassifier to be used, as a string
-	#	
-	#	input_csv_name - full path and name of an input csv file in the  
-	#		6 column format of the input test/training files
-	#
-	# Returns:
-	#	A list of "classifications". Aka: 
-	#	A list of dictionaries, one for each text, in order of lines in the 
-	#	input file. Each element is a dictionary containing the top_class
-	#	and the confidences of all the possible classes (ie the same
-	#	format as returned by classify_single_text) 
-	#	Format example:
-	#		[
-	#			{'top_class': 'class_name',
-	#		 	 'classes': [
-	#					  	{'class_name': 'myclass', 'confidence': 0.999} ,
-	#					  	{'class_name': 'myclass2', 'confidence': 0.001}
-	#						]
-	#			},
-	#			{'top_class': 'class_name',
-	#			...
-	#			}
-	#		]
-	#
-	# Error Handling:
-	#	This function should throw an exception if the classify call fails for any reason 
-	#	or if the input csv file is of an improper format.
-	#
+        # Classifies all texts in an input csv file using all classifiers for a given NLClassifier
+        # service.
+        #
+        # Inputs:
+        #       username - username for the NLClassifier to be used, as a string
+        #
+        #       password - password for the NLClassifier to be used, as a string
+        #      
+        #       input_csv_name - full path and name of an input csv file in the 
+        #              6 column format of the input test/training files
+        #
+        # Returns:
+        #       A dictionary of lists of "classifications".
+        #       Each dictionary key is the name of a classifier.
+        #       Each dictionary value is a list of "classifications" where a
+        #       "classification" is in the same format as returned by
+        #       classify_single_text.
+        #       Each element in the main dictionary is:
+        #       A list of dictionaries, one for each text, in order of lines in the
+        #       input file. Each element is a dictionary containing the top_class
+        #       and the confidences of all the possible classes (ie the same
+        #       format as returned by classify_single_text)
+        #       Format example:
+        #              {classifiername:
+        #                      [
+        #                              {'top_class': 'class_name',
+        #                              'classes': [
+        #                                        {'class_name': 'myclass', 'confidence': 0.999} ,
+        #                                         {'class_name': 'myclass2', 'confidence': 0.001}
+        #                                          ]
+        #                              },
+        #                              {'top_class': 'class_name',
+        #                              ...
+        #                              }
+        #                      ]
+        #              , classifiername2:
+        #                      [
+        #                            
+        #                      ]
+        #              
+        #              }
+        #
+        # Error Handling:
+        #       This function should throw an exception if the classify call fails for any reason
+        #       or if the input csv file is of an improper format.
+        #
 	
-	#TODO: Fill in this function
+	class_dict = {}
+	all_texts = []
 	
-	return
+	try:
+		fp = open(input_csv_name)
+	
+	except IOError:
+		print("Error opening file")
+		sys.exit()
+
+	with fp:	
+
+		data = list(csv.reader(fp))
+
+		#get classifier ids has an error handler inside the function to handle classify call fails
+		classifier_ids = get_classifier_ids(username, password)
 		
+		for classifier in classifier_ids:
+			class_dict[classifier] = []
+		
+		for rawtext in data:
+
+			#obtain the tweets from rawtext
+			text = rawtext[5]
+
+			#classify_single_text handles errors to classify_calls inside
+			for classifier2 in classifier_ids:
+				class_dict[classifier2].append(classify_single_text(username, password, classifier2, text))
+
+		return class_dict
+
 
 def compute_accuracy_of_single_classifier(classifier_dict, input_csv_file_name):
 	# Given a list of "classifications" for a given classifier, compute the accuracy of this
@@ -163,11 +263,29 @@ def compute_accuracy_of_single_classifier(classifier_dict, input_csv_file_name):
 	# Error Handling:
 	# 	This function should throw an error if there is an issue with the 
 	#	inputs.
-	#
 	
-	#TODO: fill in this function
+	correct_hit = 0
+	index = 0
 	
-	return
+	try:
+		fp = open(input_csv_file_name)
+	
+	except IOError:
+		print("Error opening file")
+		sys.exit()
+
+	with fp:
+
+		data = list(csv.reader(fp))
+		
+		for line in data:
+			if int(classifier_dict[index]["top_class"]) == int(line[0]):
+				correct_hit += 1
+			index += 1
+
+		accuracy = correct_hit / float(len(classifier_dict))
+		
+		return accuracy
 
 def compute_average_confidence_of_single_classifier(classifier_dict, input_csv_file_name):
 	# Given a list of "classifications" for a given classifier, compute the average 
@@ -201,25 +319,74 @@ def compute_average_confidence_of_single_classifier(classifier_dict, input_csv_f
 	#	See the handout for more info.
 	#
 	# Error Handling:
-	# 	This function should throw an error if there is an issue with the 
-	#	inputs.
-	#
-	
-	#TODO: fill in this function
-	
-	return
-
-
-if __name__ == "__main__":
-
-	input_test_data = '<ADD FILE NAME HERE>'
-	
-	#STEP 1: Ensure all 11 classifiers are ready for testing
+	# 	This function should thr	#STEP 1: Ensure all 11 classifiers are ready for testing
 	
 	#STEP 2: Test the test data on all classifiers
 	
 	#STEP 3: Compute the accuracy for each classifier
 	
-	#STEP 4: Compute the confidence of each class for each classifier
+	#STEP 4: Compute the confidence of each class for each classifierow an error if there is an issue with the 
+	#	inputs.
+	#
+	
+	#TODO: fill in this function
+	correct = 0.0
+	incorrect = 0.0
+	correctTotal = 0
+	incorrectTotal = 0
+	
+	index = 0
+	
+	try:
+		fp = open(input_csv_file_name)
+	
+	except IOError:
+		print("Error opening file")
+		sys.exit()
+
+	with fp:
+
+		data = list(csv.reader(fp))
+		
+		for line in data:
+			
+			if int(classifier_dict[index]["top_class"]) == int(line[0]): #sprediction is correct
+				correctTotal += 1
+				correct += float(classifier_dict[index]["classes"][0]["confidence"])
+				index += 1
+			else:
+				incorrectTotal += 1
+				incorrect += float(classifier_dict[index]["classes"][0]["confidence"])
+				index += 1
+			
+		conf_correct = correct/correctTotal
+		conf_incorrect = incorrect/incorrectTotal
+		
+		return (conf_correct, conf_incorrect)
+
+if __name__ == "__main__":
+
+	# get classifier ids, error checking inside function
+	classifier_ids = get_classifier_ids(username, password)
 	
 	
+	# assert that all classifiers are avaliable, error checking within function itself
+	assert_all_classifiers_are_available(username, password, classifier_ids)
+	
+	classifier_dict = classify_all_texts(username, password, test_data)
+
+	try:
+		f = open("4output.txt", 'w')
+	except IOError:
+		print("Error opening file")
+		sys.exit()
+
+	with f:
+	
+		for classifier in classifier_ids:
+
+			accuracy = compute_accuracy_of_single_classifier(classifier_dict[classifier], test_data)
+			confidence = compute_average_confidence_of_single_classifier(classifier_dict[classifier], test_data)
+		
+			line = str(classifier) + "accuracy: " + str(accuracy) + "confidence: " + str(confidence) +  '\n'
+			f.write(line)
--- ibmTrain.py
+++ ibmTrain.py
@@ -13,8 +13,43 @@
 ###IMPORTS###################################
 #TODO: add necessary imports
 
+import csv
+import itertools
+import re
+import requests
+import json
+import codecs
+
+###CONSTANTS#################################
+
+input_csv_name = '/u/cs401/A1/tweets/training.1600000.processed.noemoticon.csv'
+url = "https://gateway.watsonplatform.net/natural-language-classifier/api/v1/classifiers"
+
+username = "88992124-0531-4619-b710-ac512645af6a"
+password = "7HOiCMuUwmKj"
+file_size = [500, 2500, 5000]
 
 ###HELPER FUNCTIONS##########################
+
+def partition(reader, gid):
+    '''
+    Partition the reader if a group number is given
+    '''
+    new_csv = []
+
+    new_reader = list(reader)
+
+    #first half
+    idx1 = gid * 5500
+    idx2 = ((gid + 1) * (5500 - 1))
+    new_csv += new_reader[idx1:idx2]
+
+    #second half
+    idx1 = 800000 + gid * 5500
+    idx2 = 800000 + ((gid + 1) * (5500 - 1))
+    new_csv += new_reader[idx1:idx2]
+
+    return new_csv
 
 def convert_training_csv_to_watson_csv_format(input_csv_name, group_id, output_csv_name): 
 	# Converts an existing training csv file. The output file should
@@ -26,12 +61,27 @@ def convert_training_csv_to_watson_csv_format(input_csv_name, group_id, output_c
 	#
 	#	output_csv - a string containing the name of the output csv file
 	#		ex. "my_output_file.csv"
-	#
+	#`
 	# Returns:
 	#	None
+
+	# open files
 	
-	#TODO: Fill in this function
-	
+	f3 = open(input_csv_name)
+	tweet_dump = partition(csv.reader(f3), group_id)
+	f4 = open(output_csv_name, "w")
+	for ugly_tweet in tweet_dump:
+
+		polarity = ugly_tweet[0]
+		text = ugly_tweet[5]
+
+		text = text.replace("\"", '')
+		text = text.replace("\t", "\\t")
+		text = text.replace("\n", "\\n")
+		text = text.replace("\r", "\\r")
+		newline = (text + "," + polarity + '\n')
+		f4.write(unicode(newline, errors='ignore'))	
+
 	return
 	
 def extract_subset_from_csv_file(input_csv_file, n_lines_to_extract, output_file_prefix='ibmTrain'):
@@ -52,9 +102,15 @@ def extract_subset_from_csv_file(input_csv_file, n_lines_to_extract, output_file
 	#		
 	# Returns:
 	#	None
-	
-	#TODO: Fill in this function
-	
+	if n_lines_to_extract == 500:n = 500
+	elif n_lines_to_extract == 2500: n = 2500
+	elif n_lines_to_extract == 5000: n = 5000
+
+	f = open(input_csv_file)
+	fout = open(output_file_prefix + str(n) + ".csv", 'w')
+	lines = f.readlines()
+	for i in range(n_lines_to_extract):
+		fout.write(lines[i])
 	return
 	
 def create_classifier(username, password, n, input_file_prefix='ibmTrain'):
@@ -84,43 +140,34 @@ def create_classifier(username, password, n, input_file_prefix='ibmTrain'):
 	# Error Handling:
 	#	This function should throw an exception if the create classifier call fails for any reason
 	#	or if the input csv file does not exist or cannot be read.
-	#
-	
-	#TODO: Fill in this function
-	
-	return
-	
+
+	training_metadata = {'language': 'en', 'name': 'nlc {}'.format(n)}
+	data = {'training_metadata': json.dumps(training_metadata)} 
+
+    # Open will throw an IOError Exception if the input file doesn't exist or can't be read
+	with open('{}{}.csv'.format(input_file_prefix, n), 'r') as input_file:
+		files = [('training_data', input_file)]
+		response = requests.post(url, auth=(username, password), data=data, files=files)
+        print(response.json())
+        
+	if not response.ok:
+		raise Exception("The classifier could not be called")
+    
+	return response.json()
+
 if __name__ == "__main__":
 	
-	### STEP 1: Convert csv file into two-field watson format
-	input_csv_name = '<ADD FILENAME HERE>'
-	
 	#DO NOT CHANGE THE NAME OF THIS FILE
-	output_csv_name 'training_11000_watson_style.csv'
+	output_csv_name = 'training_11000_watson_style.csv'
 	
-	convert_training_csv_to_watson_csv_format(input_csv_name,output_csv_name)
-	
-	
-	### STEP 2: Save 11 subsets in the new format into ibmTrain#.csv files
-	
-	#TODO: extract all 11 subsets and write the 11 new ibmTrain#.csv files
-	#
-	# you should make use of the following function call:
-	#
-	# n_lines_to_extract = 500
-	# extract_subset_from_csv_file(input_csv,n_lines_to_extract)
+	convert_training_csv_to_watson_csv_format(input_csv_name, 88, output_csv_name)
+
+	### STEP 2: Save 3 subsets in the new format into ibmTrain#.csv files
+	extract_subset_from_csv_file(output_csv_name,500)
+	extract_subset_from_csv_file(output_csv_name,2500)
+	extract_subset_from_csv_file(output_csv_name,5000)
 	
 	### STEP 3: Create the classifiers using Watson
-	
-	#TODO: Create all 11 classifiers using the csv files of the subsets produced in 
-	# STEP 2
-	# 
-	#
-	# you should make use of the following function call
-	# n = 500
-	# username = '<ADD USERNAME>'
-	# password = '<ADD PASSWORD>'
-	# create_classifier(username, password, n, input_file_prefix='ibmTrain')
-	
-	
-	
+	for n in file_size:
+		create_classifier(username, password, n, input_file_prefix='ibmTrain')
+
